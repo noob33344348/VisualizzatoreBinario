6674f4d19c54d6e4ac863c76ee84b875f6723c22
Samuele Tomasi <samuele231106@gmail.com>
2023-05-30 16:50:50 +0200

    Aggiunta procedura unica per openClick.

---
+++ Form1.cs
@@ -0,0 +1,344 @@
+﻿using DevComponents.AdvTree;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VisualizzatoreBinario
+{
+    public partial class Form1 : Form
+    {
+        byte[] fData;
+        byte[] fData2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFile(ref byte[] fd, ref Label lb)//NEW
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            if (System.IO.File.Exists(ofd.FileName))
+            {
+                fd = System.IO.File.ReadAllBytes(ofd.FileName);
+                lb.Text = fd.Length.ToString();
+            }
+        }
+        private void ProcessFile(byte[] inData,DataGridView dgvH,DataGridView dgvD,int Header)
+        {
+            try
+            {
+                dgvH.Rows.Clear();
+                dgvH.Columns.Clear();
+                dgvD.Rows.Clear();
+                dgvD.Columns.Clear();
+                if (inData == null)
+                    return;
+                int nColonne = int.Parse(txColonne.Text);
+                int dCounter = 0;
+                int traslHeader = inData == fData ? 0 : int.Parse(txHeader2.Text) - int.Parse(txHeader.Text);
+                for (int i = 0; i < 20; i++)
+                {
+                    DataGridViewTextBoxColumn dgvc = new DataGridViewTextBoxColumn();
+                    dgvc.Width = 28;          
+                    dgvH.Columns.Add(dgvc);
+                }
+                while (dCounter < Header)
+                {
+                    DataGridViewRow dgvr = new DataGridViewRow();
+                    dgvr.HeaderCell.Value = dCounter.ToString();
+                    for (int i = 0; dCounter < Header && i < 20; i++)
+                    {
+                        if (fData.Length > dCounter && fData[dCounter] != inData[dCounter])
+                            dgvr.Cells.Add(getByteCellChanged(inData[dCounter], true));
+                        else
+                            dgvr.Cells.Add(getByteCell(inData[dCounter]));
+                        dCounter++;
+                    }
+                    dgvr.HeaderCell.Value = (dgvH.Rows.Count + 1).ToString();
+                    dgvH.Rows.Add(dgvr);
+                }
+                for (int i = 0; i < nColonne; i++)
+                {
+                    DataGridViewTextBoxColumn dgvc = new DataGridViewTextBoxColumn();
+                    dgvc.Width = 28;
+                    dgvD.Columns.Add(dgvc);
+                }
+                while (dCounter < inData.Length)
+                {
+                    DataGridViewRow dgvr = new DataGridViewRow();
+                    dgvr.HeaderCell.Value = dCounter.ToString();
+                    for (int i = 0; dCounter < inData.Length && i < nColonne; i++)
+                    {
+                        if (fData.Length + traslHeader > dCounter && fData[dCounter - traslHeader] != inData[dCounter])
+                            dgvr.Cells.Add(getByteCellChanged(inData[dCounter], true));
+                        else
+                            dgvr.Cells.Add(getByteCell(inData[dCounter]));
+                        dCounter++;
+                    }
+                    dgvr.HeaderCell.Value = (dgvD.Rows.Count + 1).ToString();
+                    dgvD.Rows.Add(dgvr);
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private DataGridViewCellStyle myRedStyle;
+        private DataGridViewCellStyle RedStyle()
+        {
+            if (myRedStyle == null)
+            {
+                myRedStyle = new DataGridViewCellStyle();
+                myRedStyle.BackColor = Color.Red;
+            }
+            return myRedStyle;
+        }
+        private DataGridViewTextBoxCell getByteCellChanged(byte data, bool changed)
+        {
+            DataGridViewTextBoxCell myCell = getByteCell(data);
+            if (changed)
+                myCell.Style = RedStyle();
+            return myCell;
+        }
+        private DataGridViewTextBoxCell getByteCell(byte data)
+        {
+            DataGridViewTextBoxCell bCell = new DataGridViewTextBoxCell();
+            if (comboBox1.Text == "Hex")
+                bCell.Value = data.ToString("X");
+            else if (comboBox1.Text == "String")
+                bCell.Value = Convert.ToChar(data);
+            else
+                bCell.Value = data.ToString();
+
+            return bCell;
+        }
+        private void txHeader_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void ProcessAll()
+        {
+            int Header = Math.Min(fData.Length, int.Parse(txHeader.Text));
+            int Header2;
+            if (!String.IsNullOrWhiteSpace(txHeader2.Text))
+                Header2 = Math.Min(fData2.Length, int.Parse(txHeader2.Text));
+            else
+                Header2 = Header;
+            idDiff.Clear();
+            int iDiff = Header2 - Header;
+            int nDiff = 0;
+            for (int i = 0; i < Math.Min(fData.Length, fData2.Length); i++)
+            {
+                if (i+ iDiff>= 0 && i+ iDiff<fData2.Length && fData[i] != fData2[i+ iDiff])
+                {
+                    idDiff.Add(i);
+                    nDiff++;
+                }
+            }
+            lbNDiff.Text = "NDiff:" + nDiff.ToString();
+            ProcessFile(fData, dgvHeader, dgvData, Header);
+            ProcessFile(fData2, dgvHeader2, dgvData2, Header2);
+        }
+        private void txColonne_TextChanged(object sender, EventArgs e)
+        { }
+        List<int> idDiff = new List<int>();
+        private void btOpen_Click(object sender, EventArgs e)
+        {
+            openFile(ref fData, ref lbF1Len);
+        }
+        private void btOpen2_Click(object sender, EventArgs e)
+        {
+            idDiff = new List<int>();
+            openFile(ref fData2, ref lbF2Len);
+        }
+        private void dgvHeader2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        { }
+        private void processSelection(DataGridView dgv)
+        {
+            try
+            {
+                if (dgv.SelectedCells.Count == 4)
+                {
+                    byte[] data = new byte[4];
+                    for (int i = 0; i < dgv.SelectedCells.Count; i++)
+                    {
+                        data[i] = byte.Parse((string)dgv.SelectedCells[i].Value);
+                    }
+                    lbInt.Text = BitConverter.ToInt32(data, 0).ToString();
+                    //lbFloat.Text = BitConverter.ToSingle(data, 0).ToString();
+                }
+                if (dgv.SelectedCells.Count == 8)
+                {
+                    byte[] data = new byte[8];
+                    for (int i = 0; i < dgv.SelectedCells.Count; i++)
+                    {
+                        data[i] = byte.Parse((string)dgv.SelectedCells[i].Value);
+                    }
+                    lbInt.Text = BitConverter.ToInt64(data, 0).ToString();
+                    //lbFloat.Text = BitConverter.ToDouble(data, 0).ToString();
+                }
+            }
+            catch(Exception ex)
+            { }
+        }
+        private void dgvHeader_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        { }
+        private void selectionChanged(object sender, EventArgs e)//NEW
+        {
+            processSelection((DataGridView)sender); 
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //ProcessFile(fData, dgvHeader, dgvData, header);
+            //ProcessFile(fData2, dgvHeader2, dgvData2);
+            ProcessAll();
+        }
+        private void btSalva1_Click(object sender, EventArgs e)
+        {
+            List<byte> b = new List<byte>();
+            foreach(DataGridViewRow r in dgvHeader.Rows)
+            {
+                foreach (DataGridViewCell c in r.Cells)
+                {
+                    if (c.Value != null)
+                        b.Add(Convert.ToByte(c.Value));
+                }
+            }
+            foreach (DataGridViewRow r in dgvData.Rows)
+            {
+                foreach (DataGridViewCell c in r.Cells)
+                {
+                    if(c.Value != null)
+                        b.Add(Convert.ToByte(c.Value));
+                }
+            }
+
+            SaveFileDialog s = new SaveFileDialog();
+            //s.Filter = "Txt|.txt";
+            s.Filter = "VDTD|.VDTD";
+            if (s.ShowDialog() != DialogResult.No)
+                System.IO.File.WriteAllBytes(s.FileName, b.ToArray());
+        }
+        private void btSalva2_Click(object sender, EventArgs e)
+        {
+            List<byte> b = new List<byte>();
+            foreach (DataGridViewRow r in dgvHeader2.Rows)
+            {
+                foreach (DataGridViewCell c in r.Cells)
+                {
+                    if (c.Value != null)
+                        b.Add(Convert.ToByte(c.Value));
+                }
+            }
+            foreach (DataGridViewRow r in dgvData2.Rows)
+            {
+                foreach (DataGridViewCell c in r.Cells)
+                {
+                    if (c.Value != null)
+                        b.Add(Convert.ToByte(c.Value));
+                }
+            }
+
+            SaveFileDialog s = new SaveFileDialog();
+            s.Filter = "Txt|.txt";
+            if (s.ShowDialog() != DialogResult.No)
+                System.IO.File.WriteAllBytes(s.FileName, b.ToArray());
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int finoA = int.Parse(txFinoA.Text);
+            int Da = int.Parse(txDa.Text);
+            byte[] newData = new byte[finoA + fData2.Length - Da];
+            int o = 0;
+            for (int i = 0; i < finoA; i++)
+            {
+                newData[o++] = fData[i];
+            }
+            for (int i = Da; i < fData2.Length; i++)
+            {
+                newData[o++] = fData2[i];
+            }
+            using (System.Windows.Forms.SaveFileDialog sfd = new SaveFileDialog())
+            {
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    System.IO.File.WriteAllBytes(sfd.FileName, newData);
+                }
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            List<byte> b = new List<byte>();
+            foreach (DataGridViewRow r in dgvHeader.Rows)
+            {
+                foreach (DataGridViewCell c in r.Cells)
+                {
+                    if (c.Value != null)
+                        b.Add(Convert.ToByte(c.Value));
+                }
+            }
+            foreach (DataGridViewRow r in dgvData.Rows)
+            {
+                foreach (DataGridViewCell c in r.Cells)
+                {
+                    if (c.Value != null)
+                        b.Add(Convert.ToByte(c.Value));
+                }
+            }
+
+            SaveFileDialog s = new SaveFileDialog();
+            string mystring = Encoding.Unicode.GetString(b.ToArray());
+            if (s.ShowDialog() != DialogResult.No)
+                System.IO.File.WriteAllText(s.FileName, mystring);
+        }
+        private void search(DataGridView data) //NEW
+        {
+            //Convert tbCerca.Text to char array
+            char[] c = new char[tbCerca.Text.Length];
+            for (int i = 0; i < c.Length; i++)
+                c[i] = tbCerca.Text[i];
+
+            char[] s = new char[c.Length];
+            foreach (DataGridViewRow row in data.Rows)
+            {
+                //Set every color to white
+                foreach (DataGridViewCell cell in row.Cells)
+                    cell.Style = new DataGridViewCellStyle() { BackColor = Color.White };
+
+
+                //Search c
+                for (int i = 0; i <= row.Cells.Count - c.Length; i++)
+                {
+                    for (int j = 0; j < c.Length; j++)
+                        s[j] = Convert.ToChar(row.Cells[i + j].Value);
+
+                    //Change color
+                    if (s.SequenceEqual(c))
+                        for (int j = 0; j < c.Length; j++)
+                            row.Cells[i + j].Style = new DataGridViewCellStyle() { BackColor = Color.LightGreen };
+                }
+            }
+        }
+        private void btCerca_Click(object sender, EventArgs e) //NEW
+        {
+            search(dgvData);
+            search(dgvData2);
+        }
+    }
+}
