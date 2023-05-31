d9002af9f07fcc4f8a92c64c731f53036b65a59c
raoul <raoulmerotto1@gmail.com>
2023-05-31 12:20:35 +0200

    yey

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -16,6 +16,7 @@ namespace VisualizzatoreBinario
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
+            {
                 components.Dispose();
             base.Dispose(disposing);
         }
